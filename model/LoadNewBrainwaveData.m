efda0cadeb90a1c2fde69881cbe317b588f588a6
Steven Hickson <me@stevenhickson.com>
2014-02-04 14:33:59 -0500

    Moved files around and added new loader

---
+++ model/LoadNewBrainwaveData.m
@@ -0,0 +1,18 @@
+%LoadBrainwaveData
+clear
+kill
+NUM_RUNS = 100;
+file1 = tdfread('/good_music.txt',',');
+%split into classes
+sober = [ file1.M, file1.A, file1.D, file1.T, file1.LA, file1.HA, file1.LB, file1.HB, file1.LG, file1.MG, file1.PS ];
+file2 = tdfread('bad_music.txt',',');
+%split into classes
+drunk = [ file2.M, file2.A, file2.D, file2.T, file2.LA, file2.HA, file2.LB, file2.HB, file2.LG, file2.MG, file2.PS ];
+
+%get rid of bad data
+%sober = WhitenData(sober);
+%drunk = WhitenData(drunk);
+sober = sober(sober(:,11) == 0, :);
+drunk = drunk(drunk(:,11) == 0, :);
+sober = sober(:,1:10);
+drunk = drunk(:,1:10);

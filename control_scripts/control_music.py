b91b643b9d16c2503fc8655272ef1d17d526e88a
Steven Hickson <me@stevenhickson.com>
2014-02-10 11:54:55 -0500

    Added clearing of old FFT data. Dumb python tab thing fixed. I don't know why they don't use {} like civilized people

--- control_scripts/control_music.py
+++ control_scripts/control_music.py
@@ -11,7 +11,7 @@ from subprocess import call
 
 
 if __name__ == '__main__':
-    NUM_BAD = 4
+    NUM_BAD = 6
     epsilon = 0.000001
     mat = scipy.io.loadmat('../model/modelBE3.mat')
     m1 = np.array(mat['m1'].newbyteorder('='))
@@ -35,64 +35,65 @@ if __name__ == '__main__':
     while(True):
         dataPoint = mindwaveDataPointReader.readNextDataPoint()
         if (not dataPoint.__class__ is RawDataPoint):
-		if (i == 0):
-			data.append(float(dataPoint.meditationValue))
-		elif (i == 1):
-			data.append(float(dataPoint.attentionValue))
-		elif (i == 2):
-			data.append(float(dataPoint.delta))
-			data.append(float(dataPoint.theta))
-			data.append(float(dataPoint.lowAlpha))
-			data.append(float(dataPoint.highAlpha))
-			data.append(float(dataPoint.lowBeta))
-			data.append(float(dataPoint.highBeta))
-			data.append(float(dataPoint.lowGamma))
-			data.append(float(dataPoint.midGamma))
-		elif (i == 3):
-			signal_val = float(dataPoint.amountOfNoise)
-			if (signal_val == 0):
-				data = np.array(data)
-				tmp = np.subtract(data,means);
-				data = np.divide(tmp,stds);
-				size = len(d)
-				if (size == 0):
-					d = data
-				else:
-					d = np.vstack((d,data))
-				if (size == 9):
-					d_fft = np.fft.fft(np.matrix(d), axis=0)
-					data = d_fft[0,:]
-					d = np.delete(d, (0), axis=0)
-					tmp = np.subtract(data,m1)
-					matrix_mult = \
-            				np.matrix(tmp) * \
-            				np.matrix(s1_inv) * \
-            				np.matrix(tmp).T
-					part2 = math.log(0.5) - 5*math.log(2*math.pi) - 0.5*lndet_sober
-        				g1=-0.5*matrix_mult + part2
-					tmp = np.subtract(data,m2)
-					matrix_mult = \
-            				np.matrix(tmp) * \
-            				np.matrix(s2_inv) * \
-            				np.matrix(tmp).T
-					part2 = math.log(0.5) - 5*math.log(2*math.pi) - 0.5*lndet_drunk
-        				g2=-0.5*matrix_mult + part2
-					decision = g1 - g2
-					if decision > 0:
-						print "Good Music"
-						num_bad = 0
-					else:
-						print "Bad Music"
-						num_bad = num_bad + 1
-						if (num_bad >= NUM_BAD):
-							call(["control-pianobar.sh","n"])
-							print("BAD SONG!!")	
-							num_bad = 0
-				else:
-					print("Low Signal Value of {0}".format(signal_val))
-		else:
-			i = 0
-			data = []
-			data.append(float(dataPoint.meditationValue))
-		i = i + 1;
+            if (i == 0):
+                data.append(float(dataPoint.meditationValue))
+            elif (i == 1):
+                data.append(float(dataPoint.attentionValue))
+            elif (i == 2):
+                data.append(float(dataPoint.delta))
+                data.append(float(dataPoint.theta))
+                data.append(float(dataPoint.lowAlpha))
+                data.append(float(dataPoint.highAlpha))
+                data.append(float(dataPoint.lowBeta))
+                data.append(float(dataPoint.highBeta))
+                data.append(float(dataPoint.lowGamma))
+                data.append(float(dataPoint.midGamma))
+            elif (i == 3):
+                signal_val = float(dataPoint.amountOfNoise)
+                if (signal_val == 0):
+                    data = np.array(data)
+                    tmp = np.subtract(data,means);
+                    data = np.divide(tmp,stds);
+                    size = len(d)
+                    if (size == 0):
+                        d = data
+                    else:
+                        d = np.vstack((d,data))
+                    if (size == 9):
+                        d_fft = np.fft.fft(np.matrix(d), axis=0)
+                        data = d_fft[0,:]
+                        d = np.delete(d, (0), axis=0)
+                        tmp = np.subtract(data,m1)
+                        matrix_mult = \
+                            np.matrix(tmp) * \
+                            np.matrix(s1_inv) * \
+                            np.matrix(tmp).T
+                        part2 = math.log(0.5) - 5*math.log(2*math.pi) - 0.5*lndet_sober
+                        g1=-0.5*matrix_mult + part2
+                        tmp = np.subtract(data,m2)
+                        matrix_mult = \
+                            np.matrix(tmp) * \
+                            np.matrix(s2_inv) * \
+                            np.matrix(tmp).T
+                        part2 = math.log(0.5) - 5*math.log(2*math.pi) - 0.5*lndet_drunk
+                        g2=-0.5*matrix_mult + part2
+                        decision = g1 - g2
+                        if decision > 0:
+                            print "Good Music"
+                            num_bad = 0
+                        else:
+                            print "Bad Music"
+                            num_bad = num_bad + 1
+                            if (num_bad >= NUM_BAD):
+                                call(["control-pianobar.sh","n"])
+                                print("BAD SONG!!")
+                                num_bad = 0
+                                d.clear()
+                else:
+                    print("Low Signal Value of {0}".format(signal_val))
+            else:
+                i = 0
+                data = []
+                data.append(float(dataPoint.meditationValue))
+            i = i + 1;
         
